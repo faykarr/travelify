d70286344d4d0e998ce2f8c3bd75e7d2132ab8d5
faykarr <nasyathfaykar@gmail.com>
2024-02-16 19:03:07 +0700

    feat: finished make hero section component

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,16 @@ module.exports = {
         orange: "#FF5722",
         dark: "#191825",
       },
+      dropShadow: {
+        btnShadow: [
+          "0px 0px 0px 0px rgba(0, 0, 0, 0.10)",
+          "0px 5px 11px 0px rgba(0, 0, 0, 0.10)",
+          "0px 20px 20px 0px rgba(0, 0, 0, 0.09)",
+          "0px 45px 27px 0px rgba(0, 0, 0, 0.05)",
+          "0px 81px 32px 0px rgba(0, 0, 0, 0.01)",
+          "0px 126px 35px 0px rgba(0, 0, 0, 0.00)",
+        ],
+      },
     },
   },
   plugins: [],
